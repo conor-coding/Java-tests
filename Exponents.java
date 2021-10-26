ff501baebba0d1900f7100a28f7a7261a5669268
conor-coding <65498907+conor-coding@users.noreply.github.com>
2021-10-26 21:17:13 +0100

    Update Exponents.java

--- Exponents.java
+++ Exponents.java
@@ -3,6 +3,7 @@ public class Exponents {
     public static void main(String[] args) {
 
         double exponent = Math.pow(2, 3);
+        
         // 2 * 2 * 2
 
         System.out.println(exponent);
