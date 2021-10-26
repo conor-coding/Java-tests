e42f265766a063ed9790707c474165fd44a2ded7
conor-coding <65498907+conor-coding@users.noreply.github.com>
2021-10-26 21:18:26 +0100

    Update ZombieKiller.java

--- ZombieKiller.java
+++ ZombieKiller.java
@@ -4,9 +4,13 @@ public class ZombieKiller {
         System.out.println("Welcome to our zombie killer program!!!!");
         
         String backpack[] = {"Shotgun", "Assault Rifle", "Sniper"};
+        
         String zombies[] = {"Close zombie", "Mid zombie", "far zombie"};
+        
         int numbersZombiesHATE[] = {4, 90, 123, 12, 4, 561, 1};
 
+        
+        
         System.out.println(numbersZombiesHATE[5]);
 
         System.out.println(backpack[2]);
